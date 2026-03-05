bee5fef01831ed37576e2b2daba37831c51d7af1
ttalggak <ttalggak@arcade.dev>
2026-03-05 21:45:07 +0900

    style: 플레이 수 라벨의 세모 아이콘(▶) 세로 정렬 수정

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ async function updatePlayCounts() {
     // 티슈 뽑기 플레이 수
     const tissueCount = await getPlayCount("tissue-pull");
     const tissueEl = document.getElementById("count-tissue");
-    if (tissueEl) tissueEl.innerText = `▶ ${tissueCount.toLocaleString()} PLAY`;
+    if (tissueEl) {
+        tissueEl.innerHTML = `<span style="display:inline-block; transform:translateY(-1px); font-size:7px;">▶</span> ${tissueCount.toLocaleString()} PLAY`;
+    }
 
     // 결재 게임 플레이 수
     const gyeoljaeCount = await getPlayCount("gyeoljae");
     const gyeoljaeEl = document.getElementById("count-gyeoljae");
-    if (gyeoljaeEl) gyeoljaeEl.innerText = `▶ ${gyeoljaeCount.toLocaleString()} PLAY`;
+    if (gyeoljaeEl) {
+        gyeoljaeEl.innerHTML = `<span style="display:inline-block; transform:translateY(-1px); font-size:7px;">▶</span> ${gyeoljaeCount.toLocaleString()} PLAY`;
+    }
 }
 
 // 페이지 로드 시 실행
