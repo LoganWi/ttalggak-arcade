c26cc936e09d7f52ba2b332fedf6365f66031d56
ttalggak <ttalggak@arcade.dev>
2026-03-10 23:29:11 +0900

    feat(soju): implement leaderboard and polish UI spacing/fonts

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore, doc, getDoc, updateDoc, setDoc, increment } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getFirestore, doc, getDoc, updateDoc, setDoc, increment, collection, query, orderBy, limit, getDocs, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 
 const firebaseConfig = {
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-export { db, doc, getDoc, updateDoc, setDoc, increment };
+export { db, doc, getDoc, updateDoc, setDoc, increment, collection, query, orderBy, limit, getDocs, serverTimestamp };
